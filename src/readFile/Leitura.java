2b9e92c7a798d610632bf735d166fd26e7b7bcf9
narfa-iscteiulpt <narfa@iscte-iul.pt>
2017-11-22 14:44:29 +0000

    Alteraçoes na aula

--- src/readFile/Leitura.java
+++ src/readFile/Leitura.java
@@ -6,13 +6,17 @@ import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import readRules.*;
+
 public class Leitura {
 
 	private File[] files;
-	
+	private int c = 0;
+	private ReadFiles readFiles;
 
-	public Leitura() {
+	public Leitura(ReadFiles readFiles) {
 
+		this.readFiles = readFiles;
 		lerFicheiros();
 	}
 
@@ -32,26 +36,56 @@ public class Leitura {
 			}
 		});
 
-		System.out.println("tamanho do files: " + files.length);
+		//System.out.println("tamanho do files: " + files.length);
 
+		
+		
 		for (int i = 0; i < files.length; i++) {
 			try {
+				
 				Scanner s = new Scanner(files[i]);
 				String line = "";
 				
-				
 				while (s.hasNextLine()) {
-					line = line + s.nextLine() + "\n";
+					c++;
+					//line = line + s.nextLine() + "\n";
+					
+					line = s.nextLine();
+					System.out.println("ver linhas " + line);
+					
 					String[] divisao = line.split("	");
 					
+					for(int j = 0; j< divisao.length; j++){
+						//System.out.println("vamos ver o que esta na divisao " + j + " " + divisao[j]);
+					}
+					//String[] divisao = s.nextLine().split("	");
 					
-					System.out.println(divisao.length);
+					//lerColuna(divisao);
+					
+					
 				}
-
+				System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////contador: " + c);
+				
 			} catch (FileNotFoundException e) {
 				System.out.println("Erro no Scanner");
 			}
 		}
 
+	}
+
+	private void lerColuna(String[] divisao) {
+		
+		
+		for(int i=1; i<divisao.length; i++){
+			String rule = divisao[i];
+			//pesquisar divisao[i] no hashMap e substituir pelo peso
+			//write no ficheiro
+			String peso = readFiles.getRegras().get(divisao[i]);
+			System.out.println("estamos aqui " + divisao[i] + " - " + peso);
+			
+			
+			
+		}
+		
 	}
 }
--- src/readFile/Main.java
+++ src/readFile/Main.java
@@ -1,9 +1,15 @@
 package readFile;
+
+import java.io.IOException;
+
+import readRules.ReadFiles;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Leitura l = new Leitura();
+	public static void main(String[] args) throws IOException {
+		 ReadFiles rf = new ReadFiles();
+		  rf.read();
+		Leitura l = new Leitura(rf);
 
 	}
 
