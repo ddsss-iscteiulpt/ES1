04827ac3ef9fef5b384914100d2109e11d507dcb
catarina <acrgs111@iscte-iul.pt>
2017-11-27 17:21:24 +0000

    mais bonito

--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -20,10 +20,10 @@ import readFile.Leitura;
 import readFile.Main;
 
 public class GUI {
-	
+
 	private JFrame frame;
 
-//Comment
+	// Comment
 
 	public GUI() {
 		super();
@@ -33,164 +33,144 @@ public class GUI {
 		buildGui();
 		frame.pack();
 	}
-	
+
 	private void buildGui() {
 		JPanel selectFiles = new JPanel();
-		
+
 		JPanel pathPanel = new JPanel();
-		pathPanel.setLayout(new GridLayout(3,1));
-		
-		for(int i = 0; i < 3; i++){
+		pathPanel.setLayout(new GridLayout(3, 1));
+
+		for (int i = 0; i < 3; i++) {
 			JTextField insertPath = new JTextField();
-			insertPath.setPreferredSize(new Dimension(1000,30));
+			insertPath.setPreferredSize(new Dimension(1000, 30));
 			pathPanel.add(insertPath);
 		}
-		
+
 		JPanel okPanel = new JPanel();
-		okPanel.setLayout(new GridLayout(3,1));
-		
-		for(int i = 0; i < 3; i++){
+		okPanel.setLayout(new GridLayout(3, 1));
+
+		for (int i = 0; i < 3; i++) {
 			JButton ok = new JButton("OK");
-			ok.setPreferredSize(new Dimension(280,30));
+			ok.setPreferredSize(new Dimension(280, 30));
 			okPanel.add(ok);
 		}
-		
+
 		selectFiles.add(pathPanel);
 		selectFiles.add(okPanel);
-		
-		frame.add(selectFiles,BorderLayout.NORTH);
-		
+
+		frame.add(selectFiles, BorderLayout.NORTH);
+
 		JPanel manualConfig = new JPanel();
-		
+
 		JPanel rulesPanel = new JPanel();
-		
+
 		JList<String> rulesList = new JList<String>();
-		rulesList.setPreferredSize(new Dimension(500,500));
+		rulesList.setPreferredSize(new Dimension(500, 500));
 		rulesPanel.add(rulesList);
-		
+
 		JTextArea weightList = new JTextArea();
-		weightList.setPreferredSize(new Dimension(500,500));
+		weightList.setPreferredSize(new Dimension(500, 500));
 		rulesPanel.add(weightList);
-		
+
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(2,1));
-		
-		
-		
+		buttonsPanel.setLayout(new GridLayout(2, 1));
+
 		JButton guardar = new JButton("Guardar");
-		guardar.setPreferredSize(new Dimension(120,30));
+		guardar.setPreferredSize(new Dimension(120, 30));
 		buttonsPanel.add(guardar);
-		
+
 		manualConfig.add(rulesPanel);
 		manualConfig.add(buttonsPanel);
-		
+
 		JPanel fpFn = new JPanel();
-		
+
 		JLabel fpLabel = new JLabel("FP");
 		fpFn.add(fpLabel);
-		
+
 		JTextField fp = new JTextField();
-		fp.setPreferredSize(new Dimension(50,50));
+		fp.setPreferredSize(new Dimension(50, 50));
 		fpFn.add(fp);
-		
+
 		JLabel fnLabel = new JLabel("FN");
 		fpFn.add(fnLabel);
-		
+
 		JTextField fn = new JTextField();
-		fn.setPreferredSize(new Dimension(50,50));
+		fn.setPreferredSize(new Dimension(50, 50));
 		fpFn.add(fn);
-		
+
 		manualConfig.add(fpFn);
-		
-		
+
 		JButton avQualidade = new JButton("Av. qualidade");
-		avQualidade.setPreferredSize(new Dimension(120,30));
+		avQualidade.setPreferredSize(new Dimension(120, 30));
 		buttonsPanel.add(avQualidade);
-		
-		avQualidade.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
+
+		avQualidade.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
 				try {
 					Main.main(null);
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 				fp.setText(Integer.toString(Leitura.FP));
 				fn.setText(Integer.toString(Leitura.FN));
 			}
 		});
-		
-		
+
 		frame.add(manualConfig, BorderLayout.CENTER);
-		
+
 		JPanel autoConfig = new JPanel();
-		
+
 		JPanel rulesPanelAuto = new JPanel();
-		
+
 		JList<String> rulesListAuto = new JList<String>();
-		rulesListAuto.setPreferredSize(new Dimension(500,500));
+		rulesListAuto.setPreferredSize(new Dimension(500, 500));
 		rulesPanelAuto.add(rulesListAuto);
-		
+
 		JTextArea weightListAuto = new JTextArea();
-		weightListAuto.setPreferredSize(new Dimension(500,500));
+		weightListAuto.setPreferredSize(new Dimension(500, 500));
 		rulesPanelAuto.add(weightListAuto);
-		
+
 		JPanel buttonsPanelAuto = new JPanel();
-		buttonsPanelAuto.setLayout(new GridLayout(2,1));
-		
+		buttonsPanelAuto.setLayout(new GridLayout(2, 1));
+
 		JButton avQualidadeAuto = new JButton("Av. qualidade");
-		avQualidadeAuto.setPreferredSize(new Dimension(120,30));
+		avQualidadeAuto.setPreferredSize(new Dimension(120, 30));
 		buttonsPanelAuto.add(avQualidadeAuto);
-		
+
 		JButton guardarAuto = new JButton("Guardar");
-		guardarAuto.setPreferredSize(new Dimension(120,30));
+		guardarAuto.setPreferredSize(new Dimension(120, 30));
 		buttonsPanelAuto.add(guardarAuto);
-		
+
 		autoConfig.add(rulesPanelAuto);
 		autoConfig.add(buttonsPanelAuto);
-		
+
 		JPanel fpFnAuto = new JPanel();
-		
+
 		JLabel fpLabelAuto = new JLabel("FP");
 		fpFnAuto.add(fpLabelAuto);
-		
+
 		JTextField fpAuto = new JTextField();
-		fpAuto.setPreferredSize(new Dimension(50,50));
+		fpAuto.setPreferredSize(new Dimension(50, 50));
 		fpFnAuto.add(fpAuto);
-		
+
 		JLabel fnLabelAuto = new JLabel("FN");
 		fpFnAuto.add(fnLabelAuto);
-		
+
 		JTextField fnAuto = new JTextField();
-		fnAuto.setPreferredSize(new Dimension(50,50));
+		fnAuto.setPreferredSize(new Dimension(50, 50));
 		fpFnAuto.add(fnAuto);
-		
+
 		autoConfig.add(fpFnAuto);
-		
+
 		frame.add(autoConfig, BorderLayout.SOUTH);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
-	public void open(){
+	public void open() {
 		frame.setVisible(true);
 	}
-	
-	
-	
-	
 
 }
